b5aef83f3f71fe002737fb8053d162e814ba9d35
Mephistopheles <worldofmagic@users.noreply.github.com>
2020-05-27 12:30:26 -0400

    add 94 95

---
+++ python/94.binary-tree-inorder-traversal.py
@@ -0,0 +1,33 @@
+#
+# @lc app=leetcode id=94 lang=python3
+#
+# [94] Binary Tree Inorder Traversal
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+
+        stack = []
+        answer = []
+
+        while True:
+            while root:
+                stack.append(root)
+                root = root.left
+            if not stack:
+                return answer
+            root = stack.pop()
+            answer.append(root.val)
+            root = root.right
+
+        
+# @lc code=end
+
