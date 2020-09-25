4cadefe642db1e0a80d3b2838d3ff89b51a68052
Mephistopheles <worldofmagic@users.noreply.github.com>
2020-09-25 13:08:47 -0400

    add greedy solution for 55

--- python/55.jump-game.py
+++ python/55.jump-game.py
@@ -6,27 +6,27 @@
 
 # @lc code=start
 class Solution:
-    def canJump(self, nums: List[int]) -> bool:
-        # f(i) = true 表达跳到了第i个位置
-        # f(i) = OR(f(j), j<i && j 能跳到i)
-        if not nums:
-            return False
-        n = len(nums)
-        dp = [False]*n
-        dp[0] = True
+    # def canJump(self, nums: List[int]) -> bool:
+    #     # f(i) = true 表达跳到了第i个位置
+    #     # f(i) = OR(f(j), j<i && j 能跳到i)
+    #     if not nums:
+    #         return False
+    #     n = len(nums)
+    #     dp = [False]*n
+    #     dp[0] = True
 
-        for i in range(1,n):
-            for j in range(i):
-                if dp[j] and j+nums[j] >= i:
-                    dp[i] = True
-                    break
-        return dp[n-1]
+    #     for i in range(1,n):
+    #         for j in range(i):
+    #             if dp[j] and j+nums[j] >= i:
+    #                 dp[i] = True
+    #                 break
+    #     return dp[n-1]
 
     def canJump(self, nums: List[int]) -> bool:
         n, rightmost = len(nums), 0
         for i in range(n):
             if i <= rightmost:
-                rightmost = max(rightmost, i + A[i])
+                rightmost = max(rightmost, i + nums[i])
                 if rightmost >= n - 1:
                     return True
         return False
